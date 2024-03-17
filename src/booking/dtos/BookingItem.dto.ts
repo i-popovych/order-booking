25cb445d66baf22de0e03e873478aba27b9b619b
Ivan Popovych <ipopovych2004@gmail.com>
2024-03-17 16:09:32 +0200

    fix: price bug

--- src/booking/dtos/BookingItem.dto.ts
+++ src/booking/dtos/BookingItem.dto.ts
@@ -1,21 +1,41 @@
 import { Exclude } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class BookingItemDto {
-  constructor(partial: any) {
+  constructor(partial: Partial<BookingItemDto>) {
     Object.assign(this, partial);
   }
+
+  @ApiProperty({ description: 'The unique identifier of the booking item' })
   readonly id: number;
 
+  @ApiProperty({ description: 'The room name' })
   room: string;
+
+  @ApiProperty({ description: 'The maximum number of guests allowed' })
   max_guests: number;
+
+  @ApiProperty({ description: 'The price per night for the booking item' })
   price_per_night: number;
+
+  @ApiProperty({ description: 'Flag indicating if WiFi is available' })
   has_wifi: boolean;
+
+  @ApiProperty({ description: 'Flag indicating if the room has a balcony' })
   has_balcony: boolean;
+
+  @ApiProperty({
+    description: 'The start date of the booking item',
+    type: Date,
+  })
   booked_from: Date | null;
+
+  @ApiProperty({ description: 'The end date of the booking item', type: Date })
   booked_to: Date | null;
 
   @Exclude()
   createdAt: Date;
+
   @Exclude()
   updatedAt: Date;
 
--- src/order/dtos/OrderItem.dto.ts
+++ src/order/dtos/OrderItem.dto.ts
@@ -4,7 +4,7 @@ import { BookingItemDto } from 'src/booking/dtos/BookingItem.dto';
 export class OrderItemDto {
   @ApiProperty({
     description: 'Sum of all booking items of orders in dollars',
-    example: '120',
+    example: 120,
   })
   price: number;
 
@@ -13,7 +13,7 @@ export class OrderItemDto {
 
     if (this.bookings.length) {
       this.price = this.bookings.reduce(
-        (acc, item) => (acc += item.price_per_night),
+        (acc, item) => (acc += Number(item.price_per_night)),
         0,
       );
     }
